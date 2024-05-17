b9111bab7f70c2784729a800e325bc4f4df8d45f
Andrea Pistelli <apisti01@gmail.com>
2024-05-17 19:37:48 +0200

    mobile header and fixed an image

--- scripts.js
+++ scripts.js
@@ -6,8 +6,10 @@ window.addEventListener("scroll", function() {
     // If scrolling up and currentScroll is greater than 0 to avoid showing the nav at the very top
     if (currentScroll < lastScrollTop && currentScroll > 0) {
         document.getElementById("appeared-nav").classList.add("sticky-nav");
+        document.getElementById("appeared-nav-mobile").classList.add("sticky-nav");
     } else {
         document.getElementById("appeared-nav").classList.remove("sticky-nav");
+        document.getElementById("appeared-nav-mobile").classList.remove("sticky-nav");
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
 }, false);
